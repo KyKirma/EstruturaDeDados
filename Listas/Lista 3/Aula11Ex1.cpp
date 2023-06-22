41666d03e7305fe386523e0cfda31a45517e7b07
KyKirma <pedrohenriquekourly@hotmail.com>
2023-06-22 10:19:47 -0300

    Update Ex1

--- Listas/Lista 3/Aula11Ex1.cpp
+++ Listas/Lista 3/Aula11Ex1.cpp
@@ -1,58 +1,40 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-
-struct no{
-	int valor;
-	no *prox;
-};
-
-
-no *insereInicio(no *lista, int valor);
-no *removerInicio(no *lista, int &valor);
-
-
-int main(void){
-	no *pilha = NULL;
-	int n, i, val;
-	
-	cout<<"Digite quantos elementos deseja inserir"<< endl;
-	cin>>n;
-	
-	for (i=0;i<n;i++){
-		cout<<"Digite o valor"<<endl;
-		cin>>val;
-		pilha = insereInicio(pilha,val);
-	}
-	
-	cout<<"Digite o valor a remover"<< endl;
-	cin>>val;
-	
-	pilha = removerInicio(pilha, val);
-	
+int main() {
+    int numeroDecimal;
+    cout << "Digite um número decimal: ";
+    cin >> numeroDecimal;
+    
+    vector<int> binario = decimalParaBinario(numeroDecimal);
+    
+    cout << "O número binário correspondente é: ";
+    for (int i = 0; i < binario.size(); i++) {
+        cout << binario[i];
+    }
+    
+    cout << endl;
+    
+    return 0;
 }
 
-
-
-no *insereInicio(no *lista, int valor){
-	no *tmp;
-	tmp = new no;
-	tmp->valor = valor;
-	tmp->prox = lista;
-	
-	lista = tmp;
-	
-	return lista;
-}
-
-no *removerInicio(no *lista, int &valor){
-	no *tmp;
-	if (lista == NULL){
-		return NULL;
-	}
-	valor = lista->valor;
-	lista = lista->prox;
-	delete tmp;
-	
-	return lista;
+vector<int> decimalParaBinario(int numero) {
+    vector<int> binario;
+    
+    if (numero == 0) {
+        binario.push_back(0);
+    }
+    else {
+        while (numero != 0) {
+            binario.push_back(numero % 2);
+            numero /= 2;
+        }
+    }
+    
+    // Inverte o vetor para obter o resultado correto
+    reverse(binario.begin(), binario.end());
+    
+    return binario;
 }
